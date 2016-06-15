351eb455fc7ba0b66b9317edd8686007eda2e016
rafael <rmendesduarte@gmail.com>
2016-06-15 16:09:54 -0300

    organize files

---
+++ approaches/CSPLDAapproach.py
@@ -0,0 +1,65 @@
+"""CSP + LDA approach.
+Implements the CSP + LDA approach using a data from the V BCI competition
+"""
+from DataManipulation import *
+from DataProcessing import *
+
+import numpy as np
+
+import os, sys, inspect
+
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split \
+	(inspect.getfile( inspect.currentframe() ))[0],'../')))
+if cmd_subfolder not in sys.path:
+    sys.path.insert(0, cmd_subfolder)
+
+### MAIN ###
+def apply_ml(DATA_CAL_PATH, CAL_EVENTS_PATH, DATA_VAL_PATH, VAL_EVENTS_PATH, 
+		SAMPLING_FREQ, LOWER_CUTOFF, UPPER_CUTOFF, FILT_ORDER, EVENT_IDS, T_MIN, T_MAX):
+
+	SMIN = T_MIN * SAMPLING_FREQ
+	SMAX = T_MAX * SAMPLING_FREQ
+
+	### MAIN ###
+
+	dp = DataFiltering()
+
+	dp.DesignFilter(LOWER_CUTOFF, UPPER_CUTOFF, SAMPLING_FREQ, FILT_ORDER)
+
+	# LOAD CALIBRATION DATA:
+	data_cal = loadDataAsMatrix(DATA_CAL_PATH).T
+	events_list_cal = readEvents(CAL_EVENTS_PATH)
+
+	data_cal = dp.ApplyFilter(data_cal)
+
+
+	# FEATURE EXTRACTION:
+	epochs_cal, labels_cal = extractEpochs(data_cal, events_list_cal, SMIN, SMAX)
+
+	dl = DataLearner()
+
+	dl.DesignLDA()
+	dl.DesignCSP(6)
+	dl.AssembleLearner()
+	dl.Learn(epochs_cal, labels_cal)
+
+	# LOAD VALIDATION DATA:
+	data_val = loadDataAsMatrix(DATA_VAL_PATH).T
+	events_list_val = readEvents(VAL_EVENTS_PATH)
+
+	data_val = dp.ApplyFilter(data_val)
+
+	# FEATURE EXTRACTION:
+	epochs_val, labels_val = extractEpochs(data_val, events_list_val, SMIN, SMAX)
+
+	dl.Evaluate(epochs_val, labels_val)
+
+	dl.PrintResults()
+
+	return dl.GetResults()
+
+
+
+
+
+
---
+++ examples/applyCSPLDA.py
@@ -0,0 +1,47 @@
+"""CSP + LDA approach.
+Implements the CSP + LDA approach using a data from the V BCI competition
+"""
+import os, sys, inspect
+
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split \
+	(inspect.getfile( inspect.currentframe() ))[0],'../approaches')))
+if cmd_subfolder not in sys.path:
+    sys.path.insert(0, cmd_subfolder)
+
+from CSPLDAapproach import *
+
+DATA_FOLDER_PATH = "/home/rafael/codes/repo/bci_training_platform/data/session/rafael3_long/"
+
+DATA_CAL_PATH = DATA_FOLDER_PATH + "data_cal.txt"
+DATA_VAL_PATH = DATA_FOLDER_PATH + "data_val.txt"
+
+ACQ_CONFIG_PATH = DATA_FOLDER_PATH + "openbci_config.txt"
+
+# EVENTS INFO PATH
+CAL_EVENTS_PATH = DATA_FOLDER_PATH + "events_cal.txt"
+VAL_EVENTS_PATH = DATA_FOLDER_PATH + "events_val.txt"
+
+SAMPLING_FREQ = 250
+
+# FILTER SPEC
+LOWER_CUTOFF = 8.
+UPPER_CUTOFF = 30.
+FILT_ORDER = 10
+
+# EPOCH EXTRACTION CONFIG:
+EVENT_IDS = dict(LH=1, RH=2)
+
+T_MIN, T_MAX = 0.5, 2.5 # time before event, time after event
+
+### MAIN ###
+
+results = apply_ml(DATA_CAL_PATH, CAL_EVENTS_PATH, DATA_VAL_PATH, VAL_EVENTS_PATH, 
+		SAMPLING_FREQ, LOWER_CUTOFF, UPPER_CUTOFF, FILT_ORDER, EVENT_IDS, T_MIN, T_MAX)
+
+print results
+
+
+
+
+
+
