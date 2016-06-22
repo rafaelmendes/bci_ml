e8b7097a26fa2ad568209e6be2e36f99428e8774
rafael <rmendesduarte@gmail.com>
2016-06-22 20:01:20 -0300

    add support to gdf files

--- approaches/CSPLDAapproach.py
+++ approaches/CSPLDAapproach.py
@@ -11,6 +11,8 @@ if cmd_subfolder not in sys.path:
 from DataManipulation import *
 from DataProcessing import *
 
+from processing_utils import loadBiosig, nanCleaner
+
 import numpy as np
 
 
@@ -18,23 +20,28 @@ import numpy as np
 def apply_ml(DATA_CAL_PATH, CAL_EVENTS_PATH, DATA_VAL_PATH, VAL_EVENTS_PATH, 
 		SAMPLING_FREQ, LOWER_CUTOFF, UPPER_CUTOFF, FILT_ORDER, CSP_N, EVENT_IDS, T_MIN, T_MAX):
 
-	SMIN = T_MIN * SAMPLING_FREQ
-	SMAX = T_MAX * SAMPLING_FREQ
-
 	### MAIN ###
-
 	dp = DataFiltering()
 
 	dp.DesignFilter(LOWER_CUTOFF, UPPER_CUTOFF, SAMPLING_FREQ, FILT_ORDER)
 
 	# LOAD CALIBRATION DATA:
-	data_cal = loadDataAsMatrix(DATA_CAL_PATH).T
+	if DATA_CAL_PATH[-3:] == 'gdf':
+		data_cal, SAMPLING_FREQ = loadBiosig(DATA_CAL_PATH).T
+		data_cal = nanCleaner(data_cal)
+
+	else:
+		data_cal = loadDataAsMatrix(DATA_CAL_PATH).T
+	
+
 	events_list_cal = readEvents(CAL_EVENTS_PATH)
 
 	data_cal = dp.ApplyFilter(data_cal)
 
-
 	# FEATURE EXTRACTION:
+	SMIN = T_MIN * SAMPLING_FREQ
+	SMAX = T_MAX * SAMPLING_FREQ
+
 	epochs_cal, labels_cal = extractEpochs(data_cal, events_list_cal, SMIN, SMAX)
 
 	dl = DataLearner()
@@ -45,7 +52,13 @@ def apply_ml(DATA_CAL_PATH, CAL_EVENTS_PATH, DATA_VAL_PATH, VAL_EVENTS_PATH,
 	dl.Learn(epochs_cal, labels_cal)
 
 	# LOAD VALIDATION DATA:
-	data_val = loadDataAsMatrix(DATA_VAL_PATH).T
+	if DATA_CAL_PATH[-3:] == 'gdf':
+		data_val, SAMPLING_FREQ = loadBiosig(DATA_VAL_PATH).T
+		data_val = nanCleaner(data)
+
+	else:
+		data_val = loadDataAsMatrix(DATA_VAL_PATH).T
+
 	events_list_val = readEvents(VAL_EVENTS_PATH)
 
 	data_val = dp.ApplyFilter(data_val)
