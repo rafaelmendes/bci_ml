15e635643b43b5290f9fb441a29d0f4f3f5ee616
rafael <rafaelmendesduarte@hotmail.com>
2016-08-31 08:40:23 -0300

    fix lda import name

--- DataProcessing.py
+++ DataProcessing.py
@@ -25,7 +25,7 @@ from pylab import plot, show, pi
 
 # from mne import Epochs, pick_types, find_events
 
-from sklearn.lda import LDA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 
 # from mne.decoding import CSP # Import Common Spatial Patterns
 from sklearn.pipeline import Pipeline
